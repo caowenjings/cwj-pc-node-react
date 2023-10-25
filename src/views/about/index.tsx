6f2f478a80ce7dd73147b5ae3ff3c0288a13869b
caowenjings <932142752@qq.com>
2023-10-25 17:00:03 +0800

    feat：添加弹框

--- src/views/about/index.tsx
+++ src/views/about/index.tsx
@@ -1,10 +1,29 @@
 /** 关于页面 */
-import React, { useState, memo } from 'react';
+import React, { useState, memo, useEffect } from 'react';
+import { message } from 'antd';
 
 interface IApp {}
 
 const App: React.FC<IApp> = (props) => {
   const [data, setData] = useState(0);
+
+  useEffect(() => {
+    document.addEventListener('visibilitychange', handleVisibilitychange);
+
+    return () => {
+      console.log(44);
+      document.removeEventListener('visibilitychange', () => {});
+    };
+  }, []);
+
+  const handleVisibilitychange = () => {
+    console.log(232323, document.visibilityState);
+    if (document.visibilityState == 'hidden') {
+      document.title = '禁止切换窗口';
+    } else if (document.visibilityState == 'visible') {
+      document.title = 'tab1';
+    }
+  };
 
   return <div>123{data}</div>;
 };
